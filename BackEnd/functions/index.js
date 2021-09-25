77935ec5605ff334caa95a524062887dfa9e3e2d
Tanzim Rayhan Ali <tanzim_rayhan@yahoo.com>
2021-09-25 01:43:29 -0400

    Added signup, login, addlinks api, set the firebase

---
+++ BackEnd/functions/index.js
@@ -0,0 +1,45 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+const cors = require("cors");
+const { db } = require("./utils/admin");
+app.use(cors());
+
+const { signup, login, addlinks, addcards } = require("./handlers/users");
+
+//users api
+app.get("/users", (req, res) => {
+  console.log("here");
+  db.collection("users")
+
+    .get()
+    .then((data) => {
+      let users = [];
+      data.forEach((doc) => {
+        users.push({
+          userId: doc.id,
+          email: doc.data().email,
+          firstName: doc.data().fName,
+          lastName: doc.data().lName,
+          title: doc.data().title,
+          address: doc.data().address,
+          phone: doc.data().phone,
+        });
+      });
+      return res.json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/signup", signup);
+app.post("/login", login);
+app.patch("/addlinks", addlinks);
+
+app.post("/addcards", addcards);
+
+exports.api = functions.https.onRequest(app);
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Digital Link Card!");
+});
---
+++ BackEnd/functions/utils/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiqGSRVFi_Kik1gC6ezeBvmCdI8LSVrdU",
+  authDomain: "digitallinkcard-687db.firebaseapp.com",
+  projectId: "digitallinkcard-687db",
+  storageBucket: "digitallinkcard-687db.appspot.com",
+  messagingSenderId: "668660954786",
+  appId: "1:668660954786:web:e74583fc572ff2b24f89fb",
+};
+
+module.exports = { firebaseConfig };
